34891cca2261e7067791fb29fc9a4b3155021676
beksbhale20 <61486131+beksbhale20@users.noreply.github.com>
2020-04-01 13:54:07 +0700

    Update bhale.php

--- bhale.php
+++ bhale.php
@@ -2,6 +2,6 @@
 // Ambil User Agent dari config sc Mastercoin
 // Buat yang belum pernah daftar. Capture dulu ambil User Agentnya agar tidak system tidak mendetek penyalahgunaan
 
-$UserAgent = "Mozilla/5.0 (Linux; Android 8.0.0; MI 5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.93 Mobile Safari/537.36";
-$Email = "xxxxxxxx@gmail.com";
-$Password = "xxxxxxxxx";
+$UserAgent = "Mozilla/5.0 (Linux; Android 8.0.0; SM-A720F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.116 Mobile Safari/537.36";
+$Email = "uberku563@gmail.com";
+$Password = "bhaleku19";
